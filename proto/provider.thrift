c61fe8e04f07e1f94cb850e06eb23fffcfc0c3a5
WWW_cool <WWW_cool@inbox.ru>
2020-10-08 15:43:29 +0300

    added provider thrift

---
+++ proto/provider.thrift
@@ -0,0 +1,37 @@
+/**
+ * Провайдеры
+ */
+
+namespace java   com.rbkmoney.fistful.provider
+namespace erlang provider
+
+include "base.thrift"
+include "fistful.thrift"
+
+/// Domain
+
+typedef base.ID ProviderID
+typedef base.ID IdentityClassID
+
+struct Provider {
+    1: required ProviderID id
+    2: required string name
+    3: required list<string> residences
+    4: required map<IdentityClassID, IdentityClass> identity_classes
+}
+
+struct IdentityClass {
+    1: required IdentityClassID id
+    2: required string name
+}
+
+///
+
+service Management {
+    Provider Get (
+        1: ProviderID id
+    )
+        throws (1: fistful.ProviderNotFound ex1)
+
+    list<Provider> List ()
+}
